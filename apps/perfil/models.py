050e2c26894bc8490b0b6828fe0ccc7b2cbf8a67
rafael candido <rafaelcandido51@gmail.com>
2024-06-13 21:59:05 -0300

    criar instancia perfil

--- apps/perfil/models.py
+++ apps/perfil/models.py
@@ -1,9 +1,13 @@
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver 
 from django.db import models
 
 class Perfil(models.Model):   
-    usuario = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='perfil') 
-    foto = models.ImageField(upload_to='perfil/foto/', default='perfil/foto-padrao.png', blank=True)  
+    usuario = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                    on_delete=models.CASCADE, related_name='perfil') 
+    foto = models.ImageField(upload_to='perfil/foto/', 
+                            default='perfil/foto-padrao.png', blank=True)  
     ocupacao = models.CharField(max_length=120, blank=True)
     descricao = models.TextField(blank=True)  
     genero = models.CharField(max_length=20, blank=True)
@@ -17,3 +21,8 @@ class Perfil(models.Model):
     class Meta:
         verbose_name = "Perfil"
         verbose_name_plural = "Perfil"
+        
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_perfil(sender, **kwargs):
+    if kwargs.get('created', False):
+        Perfil.objects.create(usuario=kwargs['instance'])
