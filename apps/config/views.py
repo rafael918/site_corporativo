db37634b6978e1cc65c4546f76e06b6e60455412
rafael candido <rafaelcandido51@gmail.com>
2024-06-22 02:11:40 -0300

    adicionar funcionalidades ao  dashbord

--- apps/config/views.py
+++ apps/config/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 @login_required
 def painel_view(request):
     return render(request, 'painel.html')
+
+@login_required
+def configuracao_view(request):
+    return render(request, 'configuracao.html')
