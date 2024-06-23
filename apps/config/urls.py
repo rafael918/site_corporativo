e33f252d551aeb43c2c3f85c9968ba0960642cf9
rafael candido <rafaelcandido51@gmail.com>
2024-06-23 19:15:19 -0300

    lista postagem perfil

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -4,4 +4,8 @@ from config import views
 urlpatterns = [
     path('', views.painel_view, name='painel'), 
     path('configuracao/', views.configuracao_view, name='configuracao'),
+    path('relatorio/', views.relatorio_view, name='relatorio'),
+    
+    
+    
 ]
--- apps/forum/views.py
+++ apps/forum/views.py
@@ -1,17 +1,35 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from base.utils import add_form_errors_to_messages
-from django.contrib.auth.decorators import login_required
 from forum.forms import PostagemForumForm
 from django.contrib import messages  
 from forum import models
 
 # lista de postagens
+#def lista_postagem_forum(request):
+#postagens = models.PostagemForum.objects.filter(ativo=True)
+#context = {'postagens': postagens}
+#return render(request, 'lista-postagem-forum.html', context)
+
 def lista_postagem_forum(request):
-    postagens = models.PostagemForum.objects.filter(ativo=True)
+    if request.path == '/forum/': # Pagina forum da home, mostrar tudo ativo.
+        postagens = models.PostagemForum.objects.filter(ativo=True)
+        template_view = 'lista-postagem-forum.html' # lista de post da rota /forum/
+    else: # Essa parte mostra no Dashboard
+        user = request.user 
+        template_view = 'dashboard/dash-lista-postagem-forum.html' # template novo que vamos criar 
+        if ['administrador', 'colaborador'] in user.groups.all() or user.is_superuser:
+            # Usuário é administrador ou colaborador, pode ver todas as postagens
+            postagens = models.PostagemForum.objects.filter(ativo=True)
+        else:
+            # Usuário é do grupo usuário, pode ver apenas suas próprias postagens
+            postagens = models.PostagemForum.objects.filter(usuario=user)
     context = {'postagens': postagens}
-    return render(request, 'lista-postagem-forum.html', context)
+    return render(request, template_view, context)
+
 
 # formulario para criar postagem
+@login_required 
 def criar_postagem_forum(request):
     form = PostagemForumForm()
     if request.method == 'POST':
@@ -44,7 +62,6 @@ def editar_postagem_forum(request, id):
             messages.warning(request, 'Seu usuario não tem permissões para acessar ess pagina')
             return redirect('postagem-forum-list')   # Redireciona para uma página de erro ou outra página adequada
 
-    
     if request.method == 'POST':
         form = PostagemForumForm(request.POST, instance=postagem)
         if form.is_valid():
@@ -57,3 +74,13 @@ def editar_postagem_forum(request, id):
     else:
         form = PostagemForumForm(instance=postagem)
     return render(request, 'form-postagem-forum.html', {'form': form})
+
+# DELETAR POSTAGEM (ID)
+@login_required 
+def deletar_postagem_forum(request, id): 
+    postagem = get_object_or_404(models.PostagemForum, id=id)
+    if request.method == 'POST':
+        postagem.delete()
+        messages.error(request, 'Seu Post '+ postagem.titulo +' foi deletado com sucesso!')
+        return redirect('lista-postagem-forum')
+    return render(request, 'detalhe-postagem-forum.html', {'postagem': postagem})
