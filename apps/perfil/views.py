11ab5de1708a9395c773810e1ad0d0fd5617d2a0
rafael candido <rafaelcandido51@gmail.com>
2024-06-25 23:00:55 -0300

    lista postagem perfil

--- apps/perfil/views.py
+++ apps/perfil/views.py
@@ -3,11 +3,10 @@ from contas.models import MyUser
 
 def perfil_view(request, username):
     filtro = MyUser.objects.select_related('perfil').prefetch_related('user_postagem_forum')
-    
-    
-    
-    
-    
-    perfil = get_object_or_404(MyUser.objects.select_related(filtro , username=username)
+    #perfil = get_object_or_404(MyUser.objects.select_related(filtro, username=username)
+    #perfil = get_object_or_404(MyUser.objects.select_related(), filtro=filtro, username=username)
+    perfil = get_object_or_404(MyUser.objects.select_related(), filtro=valor_do_filtro, username=username)
+
+
     context = {'obj': perfil} 
     return render(request, 'perfil.html', context)
